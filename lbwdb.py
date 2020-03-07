Thicket-Blender/thicket

commit b3da6243bc806e96449251f22b7c43d0c8bd7e9d

Refactor locale handling
lbwdb:
  Eliminate the alt_locale variable and just use the first two letters as
  this is primarily useful to fall back on a less specific locale. e.g. en
  instead of en-US or similar.

  Avoid having to pass locale to every get_label call by initializing the
  LaubwerkDB object with default locale.

  Laubwerk meta-data uses the en-US format while Blender reports en_US.
  Perform the translation whenever locale is passed in (init or
  get_label).

lbwbl:
  Use the Blender locale from bpy.app.translation.locale. Pass locale into
  LaubwerkDB initialization and avoid having to pass it along to every
  get_label call.

Signed-off-by: Darren Hart <dvhart@infradead.org>

lbwdb.py
@@ -12,8 +12,9 @@ import laubwerk as lbw
 
 class LaubwerkDB:
     """ Laubwerk Database Interface """
-    def __init__(self, db_filename, python=sys.executable):
+    def __init__(self, db_filename, locale="en-US", python=sys.executable):
         self.db_filename = db_filename
+        self.locale = locale.replace('_', '-')
         self.python = python
         try:
             with open(db_filename, 'r', encoding='utf-8') as f:
@@ -49,12 +50,17 @@ class LaubwerkDB:
     def update_labels(self, labels):
         self.db["labels"].update(labels)
 
-    def get_label(self, key, lang="en-US", alt_lang="en"):
+    def get_label(self, key, locale=None):
+        if locale:
+            locale = locale.replace('_', '-')
+        else:
+            locale = self.locale
+
         try:
-            if lang in self.db["labels"][key]:
-                return self.db["labels"][key][lang]
-            elif alt_lang in self.db["labels"][key]:
-                return self.db["labels"][key][alt_lang]
+            if locale in self.db["labels"][key]:
+                return self.db["labels"][key][locale]
+            elif locale[:2] in self.db["labels"][key]:
+                return self.db["labels"][key][locale[:2]]
             return key
         except KeyError:
             return key

__init__.py
@@ -36,6 +36,7 @@ from bpy.props import (BoolProperty,
                        EnumProperty,
                        )
 from bpy_extras.io_utils import ImportHelper
+from bpy.app.translations import locale
 
 bl_info = {
     "name": "Laubwerk Plants Add-on for Blender",
@@ -57,10 +58,6 @@ db_path = os.path.join(bpy.utils.user_resource('SCRIPTS', "addons", True), __nam
 plants_path = ""
 sdk_path = ""
 
-# TODO get the locale from the current blender installation via bpy.app.translations.locale.
-locale = "en_US"
-alt_locale = "en"
-
 
 # Update Database Operator (called from AddonPreferences)
 class LBWBL_OT_rebuild_db(Operator):
@@ -76,7 +73,7 @@ class LBWBL_OT_rebuild_db(Operator):
         print("  Database: %s" % db_path)
         t0 = time.time()
         lbwdb.lbwdb_write(db_path, plants_path, bpy.app.binary_path_python)  # noqa: F821
-        db = lbwdb.LaubwerkDB(db_path, bpy.app.binary_path_python)  # noqa: F821
+        db = lbwdb.LaubwerkDB(db_path, locale, bpy.app.binary_path_python)  # noqa: F821
         self.report({'INFO'}, "%s: Updated Laubwerk database with %d plants in %0.2fs" %
                     (__name__, db.plant_count(), time.time()-t0))
 
@@ -142,7 +139,7 @@ class LBWBL_Pref(AddonPreferences):
             if sdk_path not in sys.path:
                 sys.path.append(sdk_path)
             from io_import_laubwerk import lbwdb
-            db = lbwdb.LaubwerkDB(db_path, bpy.app.binary_path_python)
+            db = lbwdb.LaubwerkDB(db_path, locale, bpy.app.binary_path_python)
 
         box = self.layout.box()
         box.label(text="Laubwerk Plants Library")
@@ -185,18 +182,18 @@ class ImportLBW(bpy.types.Operator, ImportHelper):
     plant = None
 
     def model_id_callback(self, context):
-        global locale, alt_locale, db
+        global db
         items = []
         for model in ImportLBW.plant["models"].keys():
             index = ImportLBW.plant["models"][model]["index"]
-            items.append((model, db.get_label(model, locale, alt_locale), "", index))
+            items.append((model, db.get_label(model), "", index))
         return items
 
     def model_season_callback(self, context):
-        global locale, alt_locale, db
+        global db
         items = []
         for qualifier in ImportLBW.plant["models"][self.model_id]["qualifiers"]:
-            items.append((qualifier, db.get_label(qualifier, locale, alt_locale), ""))
+            items.append((qualifier, db.get_label(qualifier), ""))
         return items
 
     model_id: EnumProperty(items=model_id_callback, name="Model")
@@ -213,7 +210,7 @@ class ImportLBW(bpy.types.Operator, ImportHelper):
         return {'RUNNING_MODAL'}
 
     def draw(self, context):
-        global locale, alt_locale, db
+        global db
         layout = self.layout
         new_file = False
 
@@ -242,7 +239,7 @@ class ImportLBW(bpy.types.Operator, ImportHelper):
             self.model_season = ImportLBW.plant["models"][self.model_id]["default_qualifier"]
 
         # Create the UI entries.
-        layout.label(text="%s" % db.get_label(ImportLBW.plant["name"], locale, alt_locale))
+        layout.label(text="%s" % db.get_label(ImportLBW.plant["name"]))
         layout.label(text="(%s)" % ImportLBW.plant["name"])
         layout.prop(self, "model_id")
         layout.prop(self, "model_season")
@@ -311,7 +308,7 @@ def register():
         sys.path.append(sdk_path)
 
     from io_import_laubwerk import lbwdb
-    db = lbwdb.LaubwerkDB(db_path, bpy.app.binary_path_python)
+    db = lbwdb.LaubwerkDB(db_path, locale, bpy.app.binary_path_python)
 
 
 def unregister():

commit f806951f1dcdb4d78bf337efe9b1a5c2d3edd52f

Specify python argument to LaubwerkDB from lbwdb_write
Commit b3da6243bc80 "Refactor locale handling" added the locale argument
to the LaubwerkDB constructor, changing the position of the python
argument. This resulted in calls to rebuild the database to be called
using the default sys.executable, which is the Blender app when run
inside Blender. Attempting to run the python script from the Blender
binary resulted in unexpected output being passed into json.loads(), and
json errors being reported in the log.

Update the call from lbwdb_write to explicitly specify python=.

Fixes: b3da6243bc80 Refactor locale handling

Signed-off-by: Darren Hart <dvhart@infradead.org>

lbwdb.py
@@ -87,7 +87,7 @@ class LaubwerkDB:
 
 
 def lbwdb_write(db_filename, plants_dir, python=sys.executable):
-    db = LaubwerkDB(db_filename, python)
+    db = LaubwerkDB(db_filename, python=python)
     db.initialize()
 
     # FIXME: .gz is optional