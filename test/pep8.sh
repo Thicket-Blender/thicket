418df0e3d92bbf87e1589db11d3c6c5acbc494b9
Darren Hart <dvhart@infradead.org>
2020-06-21 14:55:12 -0700

    PEP8 Linter: Use "python3 -m" and update arguments
    Prefix flake8 with "python3 -m" to work if pip is used to install flake8
    rather than the distro package manager.
    
    Update the ignore list and drop the select list to catch more errors.
    Specifically ignore F722,F821 as all the Blender *Property declaration
    are flagged (seems to be a false positive).
    
    Add terminal description of what is being run to make the output easier
    to interpret.
    
    Drop the --count argument as it doesn't offer anything useful.
    
    Update the GitHub action to match.
    
    Signed-off-by: Darren Hart <dvhart@infradead.org>

--- test/pep8.sh
+++ test/pep8.sh
@@ -3,6 +3,15 @@
 # Run from top level project directory:
 # $ test/pep8.sh
 
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics || exit 1
+# When changing this script, be sure to sync changes with the GitHub action
+# script: .github/workflows/pythonapp.yml
+
+echo "Running critical tests"
+python3 -m flake8 . --isolated --show-source --statistics --extend-ignore=E501,F821,F722 || exit 1
+echo "PASS"
+
+echo ""
+echo "Running informational tests"
 # exit-zero treats all errors as warnings. Blender defines line length of 120.
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics
+python3 -m flake8 . --isolated --max-complexity=10 --max-line-length=120 --statistics --extend-ignore=F821,F722 && echo "PASS"
+exit 0
